fc5070925261b632a748566012b259d393cc09ef
TalMadar97 <talmadar360@gmail.com>
2024-12-31 10:52:45 +0200

    Design the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,59 @@
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand me-4" href="#" style={{ color: "black" }}>
-        B-Cards
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary shadow-sm">
+      <div className="container">
+        <a className="navbar-brand fw-bold text-white fs-1 me-5" href="#">
+          B-Cards
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" style={{ color: "black" }} href="#">
-              About <span className="sr-only"></span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" style={{ color: "black" }} href="#">
-              Favorites
-            </a>
-          </li>
-          <li className="nav-item mx-5 ">
-            <input type="search" placeholder="Search" aria-label="Search" />
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link" style={{ color: "black" }} href="/login">
-              Login
-            </a>
-          </li>
-
-          <li className="nav-item">
-            <a className="nav-link" style={{ color: "black" }} href="/register">
-              Register
-            </a>
-          </li>
-
-          <li className="nav-item mx-5 my-2">
-            <i className="fa-solid fa-id-badge"></i>
-          </li>
-        </ul>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link text-white fs-3 me-5" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white fs-3" href="#">
+                Favorites
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2 fs-5"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-light fs-5" type="submit">
+              Search
+            </button>
+          </form>
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link text-white me-3 fs-5" href="/login">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white fs-5" href="/register">
+                Register
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
