9b8aebb09ad5c8b42b04ea7871e1f9be50cbc24a
TalMadar97 <talmadar360@gmail.com>
2024-12-31 20:04:56 +0200

    Style the page not found component

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,15 +1,20 @@
+// PageNotFound.jsx
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Use useNavigate to programmatically navigate
 
   const handleGoHome = () => {
-    navigate("/"); 
+    navigate("/"); // Navigate back to the home page
   };
 
   return (
     <div className="page-not-found">
+      <div className="icon-container">
+        <i className="fa-solid fa-triangle-exclamation fa-4x"></i>{" "}
+        {/* Font Awesome 404 icon */}
+      </div>
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for doesn't exist.</p>
       <button onClick={handleGoHome} className="btn btn-primary">
