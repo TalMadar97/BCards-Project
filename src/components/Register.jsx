e306ba7184d795a5ff4da337446a08a7cccdc1e9
TalMadar97 <talmadar360@gmail.com>
2024-12-31 10:39:38 +0200

    Design Register Component

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -75,7 +75,7 @@ function Register() {
           url: values.imageUrl,
           alt: values.imageAlt,
         };
-        
+
         // Remove not allowed fields for the API
         delete payload.firstName;
         delete payload.middleName;
@@ -108,216 +108,154 @@ function Register() {
   console.log(formik.isBusiness);
   return (
     <>
-      <div className="container w-25">
-        <h4 className="display-4 my-2">Register</h4>
+      <div className="container w-md-50">
+        <h4 className="display-4 my-4 text-center">Register</h4>
         <form onSubmit={formik.handleSubmit}>
-          {/* First Name */}
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="firstName"
-              placeholder="John Doe"
-              value={formik.values.firstName}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="firstName">First Name</label>
-            {formik.touched.firstName && formik.errors.firstName && (
-              <p className="text-danger">{formik.errors.firstName}</p>
-            )}
-          </div>
-          {/* Middle Name */}
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="middleName"
-              placeholder="Middle Name"
-              value={formik.values.middleName}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="middleName">Middle Name</label>
-          </div>
-          {/* Last Name */}
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="lastName"
-              placeholder="Doe"
-              value={formik.values.lastName}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="lastName">Last Name</label>
-            {formik.touched.lastName && formik.errors.lastName && (
-              <p className="text-danger">{formik.errors.lastName}</p>
-            )}
-          </div>
-          {/* Phone */}
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="phone"
-              placeholder="Phone Number"
-              value={formik.values.phone}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="phone">Phone Number</label>
-            {formik.touched.phone && formik.errors.phone && (
-              <p className="text-danger">{formik.errors.phone}</p>
-            )}
-          </div>
-          {/* Email */}
-          <div className="form-floating mb-3">
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="name@example.com"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="email">Email Address</label>
-            {formik.touched.email && formik.errors.email && (
-              <p className="text-danger">{formik.errors.email}</p>
-            )}
-          </div>
-          {/* Password */}
-          <div className="form-floating mb-3">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="password">Password</label>
-            {formik.touched.password && formik.errors.password && (
-              <p className="text-danger">{formik.errors.password}</p>
-            )}
-          </div>
-          {/* Image URL */}
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="imageUrl"
-              placeholder="Image URL"
-              value={formik.values.imageUrl}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="imageUrl">Image URL</label>
-          </div>
-          {/* Image Alt */}
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="imageAlt"
-              placeholder="Image Alt"
-              value={formik.values.imageAlt}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="imageAlt">Image Alt</label>
-          </div>
-          {/* Address Fields */}
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="street"
-              placeholder="Street"
-              value={formik.values.street}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="street">Street</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="city"
-              placeholder="City"
-              value={formik.values.city}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="city">City</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="state"
-              placeholder="State"
-              value={formik.values.state}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="state">State</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="country"
-              placeholder="Country"
-              value={formik.values.country}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="country">Country</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="houseNumber"
-              placeholder="House Number"
-              value={formik.values.houseNumber}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="houseNumber">House Number</label>
-          </div>
-          <div className="form-floating mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="zip"
-              placeholder="Zip"
-              value={formik.values.zip}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="zip">Zip Code</label>
+          {/* Row 1: First Name and Middle Name */}
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="firstName"
+                  placeholder="John Doe"
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <label htmlFor="firstName">First Name</label>
+                {formik.touched.firstName && formik.errors.firstName && (
+                  <p className="text-danger">{formik.errors.firstName}</p>
+                )}
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="middleName"
+                  placeholder="Middle Name"
+                  value={formik.values.middleName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <label htmlFor="middleName">Middle Name</label>
+              </div>
+            </div>
           </div>
 
-          <div className="form-check mb-3">
-            <input
-              type="checkbox"
-              id="isBusiness"
-              name="isBusiness"
-              className="form-check-input"
-              checked={formik.values.isBusiness}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            <label htmlFor="isBusiness" className="form-check-label">
-              Is Business
-            </label>
+          {/* Row 2: Last Name and Phone */}
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="lastName"
+                  placeholder="Doe"
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <label htmlFor="lastName">Last Name</label>
+                {formik.touched.lastName && formik.errors.lastName && (
+                  <p className="text-danger">{formik.errors.lastName}</p>
+                )}
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="phone"
+                  placeholder="Phone Number"
+                  value={formik.values.phone}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <label htmlFor="phone">Phone Number</label>
+                {formik.touched.phone && formik.errors.phone && (
+                  <p className="text-danger">{formik.errors.phone}</p>
+                )}
+              </div>
+            </div>
           </div>
+
+          {/* Row 3: Email and Password */}
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="name@example.com"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <label htmlFor="email">Email Address</label>
+                {formik.touched.email && formik.errors.email && (
+                  <p className="text-danger">{formik.errors.email}</p>
+                )}
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <label htmlFor="password">Password</label>
+                {formik.touched.password && formik.errors.password && (
+                  <p className="text-danger">{formik.errors.password}</p>
+                )}
+              </div>
+            </div>
+          </div>
+
+          {/* Row 4: Image URL and Image Alt */}
+          <div className="row">
+            <div className="col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="imageUrl"
+                  placeholder="Image URL"
+                  value={formik.values.imageUrl}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <label htmlFor="imageUrl">Image URL</label>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="imageAlt"
+                  placeholder="Image Alt"
+                  value={formik.values.imageAlt}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                <label htmlFor="imageAlt">Image Alt</label>
+              </div>
+            </div>
+          </div>
+
+          {/* Repeat similar rows for other fields (Address, etc.) */}
+
           <button
             type="submit"
             className="btn btn-primary w-100 mb-3"
