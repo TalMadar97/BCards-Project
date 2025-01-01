297f79e6075c9e7304f935b4f7af8d9d767373c0
TalMadar97 <talmadar360@gmail.com>
2025-01-01 13:38:41 +0200

    Delete comments in page not found component

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,19 +1,18 @@
-// PageNotFound.jsx
+
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
-  const navigate = useNavigate(); // Use useNavigate to programmatically navigate
+  const navigate = useNavigate(); 
 
   const handleGoHome = () => {
-    navigate("/"); // Navigate back to the home page
+    navigate("/"); 
   };
 
   return (
     <div className="page-not-found">
       <div className="icon-container">
         <i className="fa-solid fa-triangle-exclamation fa-4x"></i>{" "}
-        {/* Font Awesome 404 icon */}
       </div>
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for doesn't exist.</p>
