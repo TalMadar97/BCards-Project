b88a66e869d8ccf1daa9a99705856f3826309063
TalMadar97 <talmadar360@gmail.com>
2025-01-02 15:18:35 +0200

    finsith styling

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -5,9 +5,11 @@ function CardsList({ cards }) {
     <>
       <div className="text-center mb-4">
         <h1>B-cards</h1>
-        <p className="text-muted">Here you can find all cards</p>
+        <p className="text-center mb-4">
+          Here you can find business cards from all categories
+        </p>
       </div>
-      
+
       <div className="container">
         <div className="row justify-content-center">
           {cards.map((card, index) => (
