6ab2086bb7356632eb061087a8f37ef1caa96a4f
TalMadar97 <talmadar360@gmail.com>
2025-01-17 18:17:15 +0200

    Fix update of image when src prop changes in ImageWithFallback component

--- src/components/Images/ImageWithFallback.jsx
+++ src/components/Images/ImageWithFallback.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ImageWithFallback = ({ src, alt, fallbackSrc, style, className }) => {
   const [imageSrc, setImageSrc] = useState(src);
@@ -6,6 +6,10 @@ const ImageWithFallback = ({ src, alt, fallbackSrc, style, className }) => {
   const handleError = () => {
     setImageSrc(fallbackSrc); // Set fallback image if the image fails to load
   };
+
+  useEffect(() => {
+    setImageSrc(src); // Update imageSrc when src prop changes
+  }, [src]);
 
   return (
     <img
